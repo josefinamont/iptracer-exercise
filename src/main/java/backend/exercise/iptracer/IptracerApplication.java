b7bcdc1540755d408003ec3d1b8b6a4c10fea5b0
Josefina Montenegro <mariajosefina.mont@gmail.com>
2020-06-28 12:21:59 +0200

    Added project template using spring boot

---
+++ src/main/java/backend/exercise/iptracer/IptracerApplication.java
@@ -0,0 +1,13 @@
+package backend.exercise.iptracer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IptracerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IptracerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/backend/exercise/iptracer/IptracerApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.exercise.iptracer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IptracerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
