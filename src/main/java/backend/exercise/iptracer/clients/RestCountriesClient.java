3c48f4060ca040db9213a1fcc35a39350028489b
Josefina Montenegro <mariajosefina.mont@gmail.com>
2020-06-29 13:34:18 +0200

    Refactor RestCountriesClient

--- src/main/java/backend/exercise/iptracer/clients/RestCountriesClient.java
+++ src/main/java/backend/exercise/iptracer/clients/RestCountriesClient.java
@@ -1,13 +1,18 @@
 package backend.exercise.iptracer.clients;
 
 import backend.exercise.iptracer.common.HttpClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RestCountriesClient extends HttpClient {
-    private String url = "https://restcountries.eu/rest/v2/alpha/";
+    @Value("${restcountries.baseUrl}")
+    private String baseUrl;
+    @Value("${restcountries.path}")
+    private String path;
 
-    public String get(String ip) {
-        return super.get(url + ip);
+    public String get(String countryCode) {
+        String url = baseUrl + path + countryCode;
+        return super.get(url);
     }
 }
