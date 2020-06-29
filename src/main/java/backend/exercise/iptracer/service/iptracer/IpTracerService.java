0d0dcb34067d73f918f979932e3f86d895ab1625
Josefina Montenegro <mariajosefina.mont@gmail.com>
2020-06-29 12:35:56 +0200

    Fixed times, now works but it needs refactor

--- src/main/java/backend/exercise/iptracer/service/iptracer/IpTracerService.java
+++ src/main/java/backend/exercise/iptracer/service/iptracer/IpTracerService.java
@@ -7,9 +7,20 @@ import backend.exercise.iptracer.service.currency.CurrencyResponse;
 import backend.exercise.iptracer.service.currency.CurrencyService;
 import backend.exercise.iptracer.service.geolocalization.IpGeolocalizationResponse;
 import backend.exercise.iptracer.service.geolocalization.IpGeolocalizationService;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
 import org.joda.time.LocalDateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.text.SimpleDateFormat;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.TimeZone;
+import java.util.stream.Collectors;
+
+import static org.joda.time.DateTimeZone.UTC;
 
 @Service
 public class IpTracerService {
@@ -36,9 +47,17 @@ public class IpTracerService {
         response.setLanguages(countryInformationResponse.getLanguages());
         response.setCurrency(countryInformationResponse.getCurrency() + " (1 " + countryInformationResponse.getCurrency()
                 + " = " + currencyResponse.getRate().toString() + " USD)");
-        response.setTimes(countryInformationResponse.getTimezones());
+        response.setTimes(countryInformationResponse.getTimezones().stream().map(IpTracerService::getUTCdatetimeAsString).collect(Collectors.toList()));
         response.setEstimatedDistance(distanceHelper.distance(countryInformationResponse.getLatlng().get(0),
                 countryInformationResponse.getLatlng().get(1)) + " kms");
         return response;
+    }
+
+    public static String getUTCdatetimeAsString(String timezone) {
+        Integer hours = timezone.split("\\+").length > 1 ? Integer.parseInt(timezone.split("\\+")[1].split(":")[0]) : 0;
+        DateTimeFormatter sdf = DateTimeFormatter.ofPattern("HH:mm:ss");
+        String zdt = ZonedDateTime.now(UTC.toTimeZone().toZoneId()).plusHours(hours).format(sdf);
+
+        return zdt + " (" + timezone + ")"; // TODO corregir
     }
 }
