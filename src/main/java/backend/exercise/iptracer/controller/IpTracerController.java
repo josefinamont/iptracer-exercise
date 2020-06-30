203e8518806c4c70e9d014511f4183dee5a46bee
Josefina Montenegro <mariajosefina.mont@gmail.com>
2020-06-30 17:28:40 +0200

    Removed unused imports

--- src/main/java/backend/exercise/iptracer/controller/IpTracerController.java
+++ src/main/java/backend/exercise/iptracer/controller/IpTracerController.java
@@ -3,15 +3,9 @@ package backend.exercise.iptracer.controller;
 import backend.exercise.iptracer.dtos.Distances;
 import backend.exercise.iptracer.dtos.IpDataDto;
 import backend.exercise.iptracer.dtos.IpTracerResponse;
-import backend.exercise.iptracer.model.exceptions.EmptyResponseException;
-import backend.exercise.iptracer.model.exceptions.InvalidFieldException;
-import backend.exercise.iptracer.model.exceptions.InvalidIpFormatException;
-import backend.exercise.iptracer.model.exceptions.UnexpectedResponseStatusException;
 import backend.exercise.iptracer.service.iptracer.IpTracerService;
 import backend.exercise.iptracer.service.statistics.StatisticsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
